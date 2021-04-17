a38a3c963ac764327e7d8638fa8aba743755f9bc
Chelsey Liu <msraxjlj@outlook.com>
2021-04-17 12:44:22 +0000

    Parallize the program using pthread

--- main.c
+++ main.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
-#include <stdarg.h>                                                             
+#include <stdarg.h>  
+#include <pthread.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+
 
 // TODO - delete this
 int DEFAULT_INPUT[9][9] = {
@@ -19,10 +24,20 @@ int DEFAULT_INPUT[9][9] = {
 #define INFO 1
 #define ERROR 2
 #define FATAL 3
+#define NUM_THREADS 27
 
 // ========= Paragram Paramters ========== //
-int LOG_LEVLE = INFO;
+const int LOG_LEVLE = INFO;
 // ======================================= //
+
+/* structure for passing data to threads */ 
+typedef struct {
+  int row;
+  int col;
+  int d_row;
+  int d_col;
+  int thread_id;
+} parameters;
 
 // TODO - change it to all zeros and fill it by the input file.
 // The input data. The program will read the input data and fill
@@ -31,6 +46,8 @@ int input[9][9] = {{0}};
 
 // Current valid result. totally 27 checks.
 int curr_valid = 0;
+
+int thread_valid[27] = {0};
 
 // Custom Log function. 
 // Only log the message whose log level >= LOG_LEVEL.
@@ -46,22 +63,23 @@ void mylog(int level, const char* fmt, ...) {
 
 // Check a grid of input[row : row + d_dow][col : col + d_col]
 // d_row * d_col should equal to 9.
-void check_grid(int row, int col, int d_row, int d_col) {
-  if (d_row * d_col != 9) {
-    mylog(ERROR, "Invalid d_row %d and d_col %d", d_row, d_col);
+void *check_grid(void* data) {
+  parameters *params = (parameters*) data;
+  if (params->d_row * params->d_col != 9) {
+    mylog(ERROR, "Invalid d_row %d and d_col %d", params->d_row, params->d_col);
     return;
   }
   // A array to count freq of each number.
   // seen[i] -> freq of number i + 1
   int seen[9] = {0};
 
-  int row_dest = row + d_row;
-  int col_dest = col + d_col;
+  int row_dest = params->row + params->d_row;
+  int col_dest = params->col + params->d_col;
   mylog(ERROR, "Check dest (%d, %d)", row_dest, col_dest);
   int i;
   int j;
-  for (i = row; i < row_dest; i++) {
-    for (j = col; j < col_dest; j++) {
+  for (i = params->row; i < row_dest; i++) {
+    for (j = params->col; j < col_dest; j++) {
       if (i < 0 || i >= 9) {
         mylog(ERROR, "Invalid row index %d", i);
         return;
@@ -82,55 +100,84 @@ void check_grid(int row, int col, int d_row, int d_col) {
   // TODO - record valid.
   mylog(INFO, "Check is %d", valid);
   curr_valid += valid;
+  thread_valid[params->thread_id] = valid;
 }
 
-// Trigger row, col and 3x3 grid checking.
-void trigger_all_checking() {
+// Trigger row, col and 3x3 grid checking parallely.
+int trigger_all_checking_parallel() {
   int row = 0;
   int col = 0;
+  
+  pthread_t threads[NUM_THREADS];
+  int thread_id = 0;
+
   for (row = 0; row < 9; ++row) {
     for (col = 0; col < 9; ++col) {
       if (row == 0) { // checking col;
+        parameters *data = (parameters *) malloc(sizeof(parameters));
+        data->row = row;
+        data->col = col;
+        data->d_row = 9;
+        data->d_col = 1;
+        data->thread_id = thread_id;
         mylog(DEBUG, "Position (%d, %d) Check col", row, col);
-        check_grid(row, col, 9, 1);
+        pthread_create(&threads[thread_id], NULL, check_grid, data);
+        thread_id++;
       }
       if (col == 0) { // Checking row;
+        parameters *data = (parameters *) malloc(sizeof(parameters));
+        data->row = row;
+        data->col = col;
+        data->d_row = 1;
+        data->d_col = 9;
+        data->thread_id = thread_id;
         mylog(DEBUG, "Position (%d, %d) Check row", row, col);
-        check_grid(row, col, 1, 9);
+        pthread_create(&threads[thread_id], NULL, check_grid, data);
+        thread_id++;
       }
       if (row % 3 == 0 && col % 3 == 0) {
+        parameters *data = (parameters *) malloc(sizeof(parameters));
+        data->row = row;
+        data->col = col;
+        data->d_row = 3;
+        data->d_col = 3;
+        data->thread_id = thread_id;
         mylog(DEBUG, "Position (%d, %d) Check 3x3 grid", row, col);
-        check_grid(row, col, 3, 3);
+        pthread_create(&threads[thread_id], NULL, check_grid, data);
+        thread_id++;
       }
     }
   }
+  int i;
+  for (i = 0; i < NUM_THREADS; i++) {
+		pthread_join(threads[i], NULL);
+	}
+
+  // All threads finished now.
+  for (i = 0; i < NUM_THREADS; i++) {
+    if (thread_valid == 0) {
+      return 0;
+    }
+  }
+  return 1;
 }
 
 void fill_input() {
-  mylog(DEBUG, "Filling input.");
   int i, j;
   for (i = 0; i < 9; i++) {
     for (j = 0; j < 9; j++) {
       input[i][j] = DEFAULT_INPUT[i][j];
     }
   }
-  mylog(DEBUG, "Fill input done.");
 }
 
-/* structure for passing data to threads */ 
-typedef struct {
-  int row;
-  int column; 
-} parameters;
 
 
 
 int main(void) {
+
   fill_input();
-  trigger_all_checking();
-  mylog(INFO, "Total valid: %d", curr_valid);
+  int valid = trigger_all_checking_parallel();
+  mylog(INFO, "Final result valid: %d", valid);
   return 0;
 }
-
-
-
