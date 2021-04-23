963ad2b30cdb7ed70c04b58283da903413161d1a
Chelsey Liu <msraxjlj@outlook.com>
2021-04-23 17:46:45 +0000

    fix bug

--- main.c
+++ main.c
@@ -7,6 +7,18 @@
 
 
 // TODO - delete this
+// int DEFAULT_INPUT[9][9] = {
+// 	{6, 2, 4, 5, 3, 9, 1, 8, 7},
+// 	{5, 1, 9, 7, 2, 8, 6, 3, 4},
+// 	{8, 3, 7, 6, 1, 4, 2, 9, 5},
+// 	{1, 4, 3, 8, 6, 5, 7, 2, 9},
+// 	{9, 5, 8, 2, 4, 7, 3, 6, 1},
+// 	{7, 6, 2, 3, 9, 1, 4, 5, 8},
+// 	{3, 7, 1, 9, 5, 6, 8, 4, 2},
+// 	{4, 9, 6, 1, 8, 2, 5, 7, 3},
+// 	{2, 8, 5, 4, 7, 3, 9, 1, 6}
+// };
+
 int DEFAULT_INPUT[9][9] = {
 	{6, 2, 4, 5, 3, 9, 1, 8, 7},
 	{5, 1, 9, 7, 2, 8, 6, 3, 4},
@@ -16,7 +28,7 @@ int DEFAULT_INPUT[9][9] = {
 	{7, 6, 2, 3, 9, 1, 4, 5, 8},
 	{3, 7, 1, 9, 5, 6, 8, 4, 2},
 	{4, 9, 6, 1, 8, 2, 5, 7, 3},
-	{2, 8, 5, 4, 7, 3, 9, 1, 6}
+	{1, 1, 1, 1, 1, 1, 1, 1, 1}
 };
 
 // LOG level.
@@ -155,7 +167,7 @@ int trigger_all_checking_parallel() {
 
   // All threads finished now.
   for (i = 0; i < NUM_THREADS; i++) {
-    if (thread_valid == 0) {
+    if (thread_valid[i] == 0) {
       return 0;
     }
   }
@@ -178,6 +190,7 @@ int main(void) {
 
   fill_input();
   int valid = trigger_all_checking_parallel();
+  mylog(INFO, "valid=%d", valid);
   if (valid) {
     mylog(INFO, "Congratulations, the sudoku board is valid!");
   } else {
